f05a5ddb4d7a5a8e66ff3b873df285096f110495
Aidan Wen <aidan.h.wen@gmail.com>
2022-12-07 21:14:59 -0800

    updates

--- data_utils.py
+++ data_utils.py
@@ -9,3 +9,10 @@ def tokenize_data(data):
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
     tokens = data.map(lambda x: {'text': tokenizer.encode(x['text'])})
     return tokens
+
+def build_pretrain_batch(dataset, seq_length, batch_size):
+    indices = list(np.random.randint(0, len(dataset), size=batch_size))
+
+    batch_input = [dataset[i:i+] for i in indices]
+
+    return batch_input
