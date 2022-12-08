24dc278324bcdfecba79fda8d4c68120244558cf
Aidan Wen <aidan.h.wen@gmail.com>
2022-12-07 22:57:54 -0800

    updated imports

--- colab_imports.py
+++ colab_imports.py
@@ -4,3 +4,16 @@
 
 data = load_dataset("oscar", "unshuffled_deduplicated_azb")
 tokens = tokenize_data(data)
+
+def build_batch(tokens, batch_size):
+    flat_tokens = list(np.array(tokens).flatten())
+    indices = list(np.random.randint(0, len(tokens), size=batch_size,dtype=int))
+    # print(indices)
+    batch = [tokens[i] for i in indices]
+
+    return batch
+
+dataset = []
+for seq in tokens['train']:
+    dataset.extend(seq)
+batch = build_batch(dataset, 512)
